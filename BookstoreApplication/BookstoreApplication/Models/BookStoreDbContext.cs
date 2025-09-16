3fd87e5fe413747f0a61a31c95798b6fa5e989ad
Josip Valtner <josipvaltner@.com>
2025-09-16 15:20:48 +0200

    Migracija V2

--- BookstoreApplication/BookstoreApplication/Models/BookStoreDbContext.cs
+++ BookstoreApplication/BookstoreApplication/Models/BookStoreDbContext.cs
@@ -14,16 +14,35 @@ namespace BookstoreApplication.Models
         public DbSet<AuthorAward> AuthorAwards { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
+        { 
+            //V1
             modelBuilder.Entity<AuthorAward>()
                 .HasOne(a => a.Author)
                 .WithMany()
-                .HasForeignKey(a => a.AuthorId);
+                .HasForeignKey(a => a.AuthorId)
+                .OnDelete(DeleteBehavior.Cascade);//V2
 
             modelBuilder.Entity<AuthorAward>()
                 .HasOne(a => a.Award)
                 .WithMany()
-                .HasForeignKey(a => a.AwardId);
+                .HasForeignKey(a => a.AwardId)
+                .OnDelete(DeleteBehavior.Cascade);//V2
+            //V2
+            modelBuilder.Entity<AuthorAward>(entity =>
+            {
+                entity.ToTable("AuthorAwardBridge");
+            });
+
+            modelBuilder.Entity<Author>()
+                .Property(a => a.DateOfBirth).HasColumnName("Birthday");
+
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Publisher)          
+                .WithMany()                        
+                .HasForeignKey(b => b.PublisherId) 
+                .OnDelete(DeleteBehavior.Restrict);
+            //V3
+
         }
     }
 }
