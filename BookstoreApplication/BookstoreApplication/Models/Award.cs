b359494700cc5ec8b36d4d09e32605c48bbbde35
Josip Valtner <josipvaltner@.com>
2025-09-16 14:19:52 +0200

    Dopunjen dome sa Awards i vezom AuthorAwards migracija v1

---
+++ BookstoreApplication/BookstoreApplication/Models/Award.cs
@@ -0,0 +1,10 @@
+﻿namespace BookstoreApplication.Models
+{
+    public class Award
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public int YearEstablished { get; set; }
+    }
+}
--- BookstoreApplication/BookstoreApplication/Models/BookStoreDbContext.cs
+++ BookstoreApplication/BookstoreApplication/Models/BookStoreDbContext.cs
@@ -10,5 +10,20 @@ namespace BookstoreApplication.Models
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Award> Awards { get; set; }
+        public DbSet<AuthorAward> AuthorAwards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AuthorAward>()
+                .HasOne(a => a.Author)
+                .WithMany()
+                .HasForeignKey(a => a.AuthorId);
+
+            modelBuilder.Entity<AuthorAward>()
+                .HasOne(a => a.Award)
+                .WithMany()
+                .HasForeignKey(a => a.AwardId);
+        }
     }
 }
